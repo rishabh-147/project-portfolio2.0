8ff4a8acfe703aa34aca305c11181c64b1083538
rishabh-147 <onlinestuff.rishabh@gmail.com>
2024-12-26 23:04:46 +0530

    Footer component

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ import Home from "./Components/Home";
 import Resume from "./Components/Resume";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 <link
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
   rel="stylesheet"
   integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
   crossorigin="anonymous"
 ></link>;
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+/>;
+
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <div className="main-container">
         {/* Header is global and visible on all routes */}
         <Header />
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css"; // Import the custom CSS for the footer
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-content">
+        {/* Copyright */}
+        <div className="footer-left">
+          <p>
+            &copy; {new Date().getFullYear()} Made with &#128153; by Rishabh
+            Tiwari
+          </p>
+        </div>
+
+        {/* Social Media Icons */}
+        <div className="footer-right">
+          <a
+            href="https://github.com/rishabh-147"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-2"
+          >
+            <i className="fab fa-github fa-2x"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tiwarirt147"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-2"
+          >
+            <i className="fab fa-linkedin fa-2x"></i>
+          </a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-2"
+          >
+            <i className="fab fa-twitter fa-2x"></i>
+          </a>
+          {/* Add more social links as needed */}
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
