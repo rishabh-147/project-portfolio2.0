c86ad5ab77bdc89ccd8c2ea60146f498be561a1c
rishabh-147 <onlinestuff.rishabh@gmail.com>
2024-12-27 02:58:39 +0530

    BrowserRoute to HashRouter for the GitHub pages Roouting issue and moved link tags to index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Home from "./Components/Home";
@@ -10,25 +10,16 @@ import Footer from "./Components/Footer";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Project from "./Components/Project";
 
-<link
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-  rel="stylesheet"
-  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-  crossorigin="anonymous"
-></link>;
-<link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-/>;
-
 function App() {
   return (
-    <BrowserRouter>
+    <Router basename="/project-portfolio2.0">
+      {" "}
+      {/* Keep this for the relative URL */}
       <div className="main-container">
         {/* Header is global and visible on all routes */}
         <Header />
 
-        <div className="bodyWrapper custom-css">
+        <div className="bodyWrapper">
           {/* Define routes here */}
           <Routes>
             {/* Define actual routes */}
@@ -37,7 +28,6 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/project" element={<Project />} />
-
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
@@ -45,7 +35,7 @@ function App() {
         {/* Footer is global and visible on all routes */}
         {/* <Footer /> */}
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
