5d9da79e12b26a5ba086ab0acc9c4ab372d73f40
rishabh-147 <onlinestuff.rishabh@gmail.com>
2024-12-31 00:54:18 +0530

    Routing issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Home from "./Components/Home";
