51e9a60dbe6bbfd487a2fd6f3f3cc0cdbb019909
rishabh-147 <onlinestuff.rishabh@gmail.com>
2025-01-02 21:49:37 +0530

    `Refactor project portfolio: update package.json, CSS, and Resume.js`

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,30 +1,38 @@
-import React, { useEffect } from "react";
-import './Resume.css';
+import React from "react";
+import "./Resume.css";
 import Footer from "./Footer";
 
 const Resume = () => {
+  const onButtonClick = () => {
+    const link = document.createElement("a");
+    link.href = "/Rishabh_Tiwari.pdf"; // Direct path to the file in the public directory
+    link.download = "Rishabh_Tiwari.pdf";
+    link.click();
+  };
 
   return (
     <div className="resume-page">
       <div className="resume-card">
         <h1>My Resume</h1>
         <p>Welcome to my resume page. Feel free to download my resume:</p>
-        
+
         {/* Google Drive Link */}
         <div className="download-link">
-          <a href="https://drive.google.com/file/d/1EcNQDkuI-O-uRmBaxShCgeAx5lOfSoJi/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://drive.google.com/file/d/1EcNQDkuI-O-uRmBaxShCgeAx5lOfSoJi/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Download Resume from Google Drive (PDF)
           </a>
         </div>
 
-        {/* Fallback to download directly from the public folder */}
+        {/* Direct download from the public folder */}
         <div className="download-link temp-Down">
-          <a href="#" download>
-            Download Resume (PDF)
-          </a>
+          <a onClick={onButtonClick}>Download Resume (PDF)</a>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
