1fb5380e7367adb0f650621603720b797683f2dd
rishabh-147 <onlinestuff.rishabh@gmail.com>
2025-05-04 01:33:03 +0530

    updated homepage for the portal

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,20 +8,21 @@ const Home = () => {
     <div className="home-container">
       <div className="home-content">
         <div className="profile-img-container">
-          {/* Profile Image */}
           <img src={userProfile} alt="Profile" className="profile-img" />
         </div>
 
         <div className="bio">
-          <h1>Hi, I'm Rishabh Tiwari</h1>
+          <h1>👋 Hi, I'm Rishabh Tiwari!</h1>
           <p>
             I’m a software developer at{" "}
             <span className="strong-text">
               <u>
-                <a className="a-link" href="https://www.mindgate.solutions/">Mindgate Solutions</a>
+                <a className="a-link" href="https://www.mindgate.solutions/">
+                  Mindgate Solutions
+                </a>
               </u>
             </span>
-            , currently working with the{" "}
+            , currently working on an exciting project with the{" "}
             <span className="strong-text">
               <u>
                 <a className="a-link" href="https://www.npci.org.in/">
@@ -29,15 +30,17 @@ const Home = () => {
                 </a>
               </u>
             </span>{" "}
-            on the{" "}
+            - working on the{" "}
             <span className="strong-text">
               <u>
                 <a className="a-link" href="https://www.linkedin.com/pulse/understanding-nocs-reconciliation-settlement-platform-ondc/">
-                  NOCS
+                  NOCS 
                 </a>
               </u>
             </span>{" "}
-            project. NOCS, launched by{" "}
+            project! 
+            <br />
+            NOCS, launched by{" "}
             <span className="strong-text">
               <u>
                 <a className="a-link" href="https://www.npci.org.in/who-we-are/group-companies/npci-bharat-billpay-ltd/bharat-billpay-overview">
@@ -48,14 +51,16 @@ const Home = () => {
             , handles reconciliation and settlement for transactions within the{" "}
             <span className="strong-text">
               <u>
-                <a className="a-link" href="https://ondc.org/ondc-buyer-apps/">ONDC</a>
+                <a className="a-link" href="https://ondc.org/ondc-buyer-apps/">
+                  ONDC 
+                </a>
               </u>
             </span>{" "}
-            network. Additionally, I contribute to NBBL’s broader efforts in
-            supporting the bill payment ecosystem across India.
+            network.
             <br />
-            <span className="strong-text">
-              <u>Let's connect and collaborate!</u>
+            I’m passionate about leveraging technology to create real-world impact, and I also contribute to NBBL’s broader efforts in supporting the bill payment ecosystem across India.{" "}
+            <br/><span className="strong-text">
+              <u>Let’s connect and collaborate! 🤝</u>
             </span>
           </p>
         </div>
@@ -63,32 +68,16 @@ const Home = () => {
         {/* Button Container at Bottom */}
         <div className="button-container">
           <Link to="/resume" className="button">
-            {/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4 2H16L20 6V22H4V2Z" stroke="#4A90E2" strokeWidth="2" />
-            <path d="M12 2V12H20" stroke="#4A90E2" strokeWidth="2" />
-          </svg> */}
-            Resume
+            Resume 📝
           </Link>
           <Link to="/project" className="button">
-            {/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 2L15 5H9L12 2Z" fill="#4A90E2" />
-            <rect x="5" y="7" width="14" height="14" stroke="#4A90E2" strokeWidth="2" />
-          </svg> */}
-            Projects
+            Projects 💻
           </Link>
           <Link to="/about" className="button">
-            {/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="10" stroke="#4A90E2" strokeWidth="2" />
-            <path d="M7 12L12 7L17 12" stroke="#4A90E2" strokeWidth="2" />
-          </svg> */}
-            About Me
+            About Me 📚
           </Link>
           <Link to="/contact" className="button">
-            {/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="2" y="4" width="20" height="16" stroke="#4A90E2" strokeWidth="2" />
-            <circle cx="12" cy="12" r="5" stroke="#4A90E2" strokeWidth="2" />
-          </svg> */}
-            Contact
+            Contact 📞
           </Link>
         </div>
       </div>
